a18d211eefbc4f02624bffc54565ce97b980b4c0
Cprakhar <cprakhar21@iitk.ac.in>
2023-03-09 04:24:13 +0530

    Add Search feature and modify

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { query } from 'express';
 import _ from 'lodash';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
@@ -10,56 +10,26 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-connect('mongodb+srv://Pchhalotre:Sonu321chh@cluster0.sjvnsih.mongodb.net/treesData');
+connect('mongodb+srv://Pchhalotre:Sonu321chh@cluster0.sjvnsih.mongodb.net/treesDB');
 
 const Schema = {
     scientific_name: String,
     category: String,
     location: String,
     common_name: String,
-    img_src: String,
-    descrp: String
+    img_src: String
 };
 
-const Tree = model('treesInfo', Schema);
+const Tree = model('treeInfo', Schema);
 
 const bg_fruit = '/images/fruit_bg.jpg';
 const bg_flower = '/images/flower_bg.jpg';
 const bg_medicinal = '/images/medicinal_bg.jpg';
 const bg_cactus = '/images/cactus_bg.jpeg';
 const bg_ornamental = '/images/ornamental_bg.jpg'; 
-// const FruitTree = model('FruitTree', Schema);
-// const FloweringTree = model('FloweringTree', Schema);
-// const VegetableTree = model('VegetableTree', Schema);
+const bg_main = '/images/Title-img.jpg';
+const bg_vegetable = '/images/vegetable_bg.jpg';
 
-
-// const flowering = [
-//     {
-//         name: 'Pinwheel',
-//         img_src: 'flowering_images/pinwheel.jpg'
-//     }
-// ]
-
-// const medicinal = [
-//     {
-//         name: 'Pinwheel',
-//         img_src: 'flowering_images/pinwheel.jpg'
-//     }
-// ]
-
-// const fruit = [
-//     {
-//         name: 'Pinwheel',
-//         img_src: 'flowering_images/pinwheel.jpg'
-//     }
-// ]
-
-// const vegetable = [
-//     {
-//         name: 'Pinwheel',
-//         img_src: 'flowering_images/pinwheel.jpg'
-//     }
-// ]
 
 app.route('/')
     .get((req, res) =>{
@@ -106,6 +76,55 @@ app.route('/flowering')
             console.log(err);
         }
     }); 
+
+app.route('/ornamental')
+    .get(async (req, res) =>{
+        const foundItems = await Tree.find({category: 'Ornamental'});
+        try{
+            res.render('category', {Category: 'Ornamental Plants/Trees', Items: foundItems, bg: bg_ornamental});
+        }catch(err){
+            console.log(err);
+        }
+    }); 
+
+app.route('/vegetable')
+    .get(async (req, res) =>{
+        const foundItems = await Tree.find({category: 'Vegetable'});
+        try{
+            res.render('category', {Category: 'Vegetable Plants/Trees', Items: foundItems, bg: bg_vegetable});
+        }catch(err){
+            console.log(err);
+        }
+    });        
+
+app.route('/find/:name')
+    .get(async (req, res) =>{
+        const name = req.params.name;
+        const foundItems = await Tree.find({common_name: name});
+        try{
+            res.render('find', {Items: foundItems, bg: bg_main});
+        }catch (err){
+            console.log(err);
+        }
+    })
+
+app.route('/search')
+    .get(async(req, res) =>{
+        const query = req.query.query;
+        const results = await Tree.find({
+            $or: [
+              { common_name: { $regex: query, $options: 'i' } },
+              { scientific_name: { $regex: query, $options: 'i' } },
+              { category: { $regex: query, $options: 'i' } },
+              { location: { $regex: query, $options: 'i' } }
+            ]
+          });
+          try{
+            res.render('find', {Items: results, bg: bg_main})
+          }catch (err){
+            console.log(err);
+          }
+    })
 
 
 app.listen(3000, function(){
